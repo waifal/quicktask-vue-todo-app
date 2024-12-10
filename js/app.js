25693b27d8f6ba5f1e22a8e6871aed2a7383b609
WAI <waifalcode@gmail.com>
2024-12-10 19:02:26 +1300

    Add method to add tasks from input value to the body section

--- js/app.js
+++ js/app.js
@@ -2,11 +2,23 @@ const app = Vue.createApp({
     data() {
         return {
             title: "QuickTask",
+            tasks: [],
         };
+    },
+
+    methods: {
+        submitTask() {
+            if (input.value.trim() !== "") {
+                this.tasks.push(input.value);
+                input.value = "";
+            }
+        },
     },
 });
 
 app.mount("#app");
+
+const input = document.getElementById("input-task");
 
 const images = document.querySelectorAll("img");
 for (const img of images) {
